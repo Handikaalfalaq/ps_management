f21dd4637c08d61ae540455ea06a5993dc63d466
handika <handika.alfalaq@tsmakmur.co.id>
2024-07-23 17:34:45 +0700

    update home

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('home');
+});
+
+// Route::get('/dataTransaksi', function () {
+//     return view('dataTransaksi');
+// });
+
+// Route::get('/laporanTransaksi', function () {
+//     return view('laporanTransaksi');
+// });
+
+// Route::get('/dataMember', function () {
+//     return view('dataMember');
+// });
+
+// Route::get('/dataPerangkat', function () {
+//     return view('dataPerangkat');
+// });
+
+// Route::get('/dataPetugas', function () {
+//     return view('dataPetugas');
+// });
+
+// Route::get('/setingApp', function () {
+//     return view('setingApp');
+// });
