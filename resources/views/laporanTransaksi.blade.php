d3486e9b9a1425e4f725a78b55e92af88877cbff
handika <handika.alfalaq@tsmakmur.co.id>
2024-07-23 22:18:24 +0700

    update navbar

--- resources/views/laporanTransaksi.blade.php
+++ resources/views/laporanTransaksi.blade.php
@@ -1,4 +1,4 @@
 <x-layout>
     <x-slot:title>{{ $title }}</x-slot:title>
-    <h3 class="text-xl">ini Halaman Home</h3>
+    <h3 class="text-xl">ini laporan transaksi</h3>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -3,29 +3,29 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', ['title' => 'Home Page']);
 });
 
-// Route::get('/dataTransaksi', function () {
-//     return view('dataTransaksi');
-// });
+Route::get('/dataTransaksi', function () {
+    return view('dataTransaksi', ['title' => 'Data Transaksi Page']);
+});
 
-// Route::get('/laporanTransaksi', function () {
-//     return view('laporanTransaksi');
-// });
+Route::get('/laporanTransaksi', function () {
+    return view('laporanTransaksi', ['title' => 'Laporan Transaksi Page']);
+});
 
-// Route::get('/dataMember', function () {
-//     return view('dataMember');
-// });
+Route::get('/dataMember', function () {
+    return view('dataMember', ['title' => 'Data Member Page']);
+});
 
-// Route::get('/dataPerangkat', function () {
-//     return view('dataPerangkat');
-// });
+Route::get('/dataPerangkat', function () {
+    return view('dataPerangkat', ['title' => 'Data Perangkat Page']);
+});
 
-// Route::get('/dataPetugas', function () {
-//     return view('dataPetugas');
-// });
+Route::get('/dataPetugas', function () {
+    return view('dataPetugas', ['title' => 'Data Petugas Page']);
+});
 
-// Route::get('/setingApp', function () {
-//     return view('setingApp');
-// });
+Route::get('/settingApp', function () {
+    return view('settingApp', ['title' => 'Setting App Page']);
+});
